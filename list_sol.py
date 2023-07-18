eafea0d23fe34c7053c961e9444e0571bd3b4b57
TSampadaA <60322606+TSampadaA@users.noreply.github.com>
2023-07-18 17:02:53 -0500

    Update list_sol.py

--- list_sol.py
+++ list_sol.py
@@ -9,3 +9,37 @@ elif (breed == dogBreeds[0]):
     print("That's my favorite dog too!")
 else:
     print("Nice.")
+
+
+
+
+import random
+
+randomInt = random.randint(1, 100)
+guess = int(input("Pick a number between 1 and 100\n"))
+
+while (guess != randomInt): 
+    if (guess < randomInt): 
+        guess = int(input("Your guess was too low. Pick again!\n"))
+    else: 
+        guess = int(input("Your guess was too high. Pick again!\n"))
+
+print("Good job! You guessed correctly!")
+--------------------------------------------------------
+import random
+
+randomInt = random.randint(1, 10)
+guess = int(input("Pick a number between 1 and 100\n"))
+numGuesses = 1
+
+while (guess != randomInt and numGuesses < 10): 
+    if (guess < randomInt): 
+        guess = int(input("Your guess was too low. Pick again!\n"))
+    else: 
+        guess = int(input("Your guess was too high. Pick again!\n"))
+    numGuesses = numGuesses + 1
+
+if (guess == randomInt):
+    print("Good job! You guessed the random number with only " + str(numGuesses) + " guesses!")
+else:
+    print("You ran out of guesses.")
