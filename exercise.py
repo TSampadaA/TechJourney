b0a4fab8faebd46e396f46cdc424487e6ad2db01
TSampadaA <60322606+TSampadaA@users.noreply.github.com>
2023-07-18 17:01:23 -0500

    Create exercise.py

---
+++ exercise.py
@@ -0,0 +1,11 @@
+Create a list of your favourite things. Ask the user for their favorite out of that. If the item is not in the lis,
+tell the user you’re not familiar with that. If the item is the first in your list, let them know it’s your favorite too.
+Otherwise, just print something.
+
+
+Ask the user for a number between 1 and 100. Create a random number.
+While the user hasn’t correctly guessed the number, tell them if their guess was higher or lower. Ask them for a new guess and loop again. 
+Congratulate them when they win! 
+**If extra time: keep track of how many guesses it took the user. Or limit the user to a certain number of guesses.**
+                                                                                                                                 
+                                                                                                                                 
