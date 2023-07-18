f66ea743a79db6f6e78953a800729707a4baee85
TSampadaA <60322606+TSampadaA@users.noreply.github.com>
2023-07-18 17:03:26 -0500

    Update exercise.py

--- exercise.py
+++ exercise.py
@@ -9,5 +9,8 @@ Congratulate them when they win!
 
 
 **If extra time: keep track of how many guesses it took the user. Or limit the user to a certain number of guesses.**
-                                                                                                                                 
-                                                                                                                                 
+
+
+Create a program that asks the user how many times a coin will be heads when flipped 100 times. 
+Use a loop to flip the coin 100 times and generate a random number between 0 and 1 to determine heads or tails. 
+When the loop is done, print out the number of times the coin was heads vs tails. Let the user know if their guess was correct!
